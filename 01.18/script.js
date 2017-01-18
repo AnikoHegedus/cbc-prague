508d4d5597b51f82ba1b4f63d3b51391ebf39bf0
Unknown <millkamill@hotmail.com>
2017-01-18 12:42:50 +0100

    18 Jan

---
+++ 01.18/script.js
@@ -0,0 +1,23 @@
+/*alert("Hello World!");*/
+
+var value = null;
+var type = null;
+
+/*value = '123';
+type = typeof value;
+
+alert(type);*/
+
+/*123
+'123 Times Square'
+12.3
+true
+'true'
+[1, 2, 3]
+{ foo: 'bar' }
+document*/
+
+var message_element = document.getElementById("message");
+
+console.log(message_element);
+console.log(typeof message_element);
